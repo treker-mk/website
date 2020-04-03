8c450c41f9bfbe12b664f02517d1f158b724d806
Andraz Vrhovec <andraz@vrhovec.si>
2020-04-03 19:34:16 +0000

    Deploy preview.sledilnik.org to github.com/slo-covid-19/website.git:gh-pages

---
+++ table-region-order/js/chunk-2d0c0a81.b088840f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c0a81"],{"435a":function(t,n){var a="<h1>FAQ</h1> <details> <summary><h2>FAQ fact 1</h2></summary> Content Content Content </details> <details> <summary><h3>FAQ fact 2</h3></summary> <p>Content</p> <p>Content</p> <p>Content</p> </details>";t.exports=a}}]);
+//# sourceMappingURL=chunk-2d0c0a81.b088840f.js.map
